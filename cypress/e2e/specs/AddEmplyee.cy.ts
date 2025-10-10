38e1e75af265c96b65ec96dbe91b25174d96e12c
Rayaalislam Salem <120057726+rayasalem@users.noreply.github.com>
2025-10-11 01:54:22 +0300

    Update AddEmplyee.cy.ts

--- cypress/e2e/specs/AddEmplyee.cy.ts
+++ cypress/e2e/specs/AddEmplyee.cy.ts
@@ -1,27 +1,44 @@
 import LoginPage from "cypress/support/pages/LoginPage";
-import AddEmployee from "cypress/support/pages/PIM/AddEmployee";
 import { CREDENTIALS, EMPLOYEE } from "cypress/support/helper/Constant";
 
 const login = new LoginPage();
-const addEmployee = new AddEmployee();
 
-describe('OrangeHRM - Delete second employee then add a new one', () => {
+describe('OrangeHRM - Search by Employee ID, delete if exists then add new employee', () => {
   beforeEach(() => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   });
 
-  it('should delete second employee if exists, then add a new employee with login details', () => {
+  it('should search by Employee ID, delete if exists, then add new employee', () => {
     login.login(CREDENTIALS.valid.username, CREDENTIALS.valid.password);
     cy.url().should('include', '/dashboard');
     cy.contains('PIM').should('be.visible');
 
-    addEmployee.addNewEmployee(
-      EMPLOYEE.username,
-      EMPLOYEE.password,
-      EMPLOYEE.firstName,
-      EMPLOYEE.lastName,
-      EMPLOYEE.employeeId,
-      EMPLOYEE.fullName
-    );
+    cy.contains('PIM').click();
+    cy.contains('Employee List').click();
+
+    cy.get('input.oxd-input.oxd-input--active').eq(1).clear().type(EMPLOYEE.employeeId);
+    cy.get('button[type="submit"]').filter(':visible').first().click();
+
+    cy.get('div.oxd-table-body').then($body => {
+      const rows = $body.find('.oxd-table-row');
+      if (rows.length > 0) {
+        cy.wrap(rows.eq(0)).find('.oxd-icon.bi-trash').click({ force: true });
+        cy.get('button.oxd-button--label-danger').contains('Yes, Delete').click();
+        cy.contains('Successfully Deleted').should('be.visible');
+      }
+    });
+
+    cy.contains('button', 'Add').click();
+    cy.get('input[placeholder="First Name"]').type(EMPLOYEE.firstName);
+    cy.get('input[placeholder="Last Name"]').type(EMPLOYEE.lastName);
+    cy.get('input.oxd-input.oxd-input--active').eq(3).clear().type(EMPLOYEE.employeeId);
+
+    cy.get('input[type="checkbox"]').first().check({ force: true });
+    cy.get('.oxd-input[autocomplete="off"]').eq(0).type(EMPLOYEE.username);
+    cy.get('.oxd-input[autocomplete="off"]').eq(1).type(EMPLOYEE.password);
+    cy.get('.oxd-input[autocomplete="off"]').eq(2).type(EMPLOYEE.password);
+
+    cy.contains('button', 'Save').click();
+    cy.contains(`${EMPLOYEE.firstName} ${EMPLOYEE.lastName}`, { timeout: 10000 }).should('be.visible');
   });
 });
